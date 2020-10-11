95d0ae55af21938204deccf1bf61f1a420c0e98d
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2020-10-11 08:02:18 +0200

    Tests

--- queens/src/main/scala/queens/Main.scala
+++ queens/src/main/scala/queens/Main.scala
@@ -3,14 +3,10 @@ package queens
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric._
 
+/*
 import cats._
 import cats.data._
 import cats.implicits._
-
-
-/*
-scala> refineV[Positive](x)
-res1: Either[String, Int Refined Positive] = Right(42)
 */
 
 
@@ -24,22 +20,18 @@ object Queen {
   def diagonal(q1: Queen, q2: Queen): Boolean = 
     return Math.abs(q1.x.value - q1.y.value) == Math.abs(q2.x.value - q2.y.value)
 
-  def check(q1: Queen, q2: Queen): Boolean = {
-      if(q1.x == q2.x) return true
-      if(q1.y == q2.y) return true
-      if(diagonal(q1, q2)) return true
-      return true
-  }
+  def check(q1: Queen, q2: Queen): Boolean = diagonal(q1, q2)
   
   def check(jugada: Jugada): Boolean = {
-    var tuples:List[(Queen, Queen)] = for {
+    val tuples:List[(Queen, Queen)] = for {
       q1 <- jugada.jugada
-      q2 <- jugada.jugada
+      q2 <- jugada.jugada.filter(_ != q1)
     } yield (q1, q2)
+
     tuples.foldLeft(false)((b, qs) => b match {
       case true => true
       case false => check(qs._1, qs._2)
-    })
+      })
   }
 }
 
--- queens/src/test/scala/queens/QueenSpec.scala
+++ queens/src/test/scala/queens/QueenSpec.scala
@@ -2,26 +2,35 @@ package queens
 
 import org.scalatest.flatspec.AnyFlatSpec
 
-import cats._
-import cats.data._
+//import cats._
+//import cats.data._
 import cats.implicits._
 
 import eu.timepit.refined._
 import eu.timepit.refined.auto._
 
-import eu.timepit.refined.cats._
-import eu.timepit.refined.types.string._
+//import eu.timepit.refined.cats._
+//import eu.timepit.refined.types.string._
 import org.scalatest.matchers.should.Matchers
 
 class QueenSpec extends AnyFlatSpec with Matchers {
-    "DiagonalUtil" should "work diagonal" in {
+    "Diagonal" should "work diagonal" in {
         Queen.diagonal(Queen(1, 1), Queen(2, 2)) shouldBe true
         Queen.diagonal(Queen(1, 2), Queen(2, 3))  shouldBe true
         Queen.diagonal(Queen(1, 2), Queen(5, 3))  shouldBe false
         Queen.diagonal(Queen(1, 2), Queen(6, 7)) shouldBe true
     }
-}
 
+    "Diagonal" should "inform check" in {
+        Queen.check( Jugada( List(
+            Queen(0, 0), Queen(0, 1), Queen(0, 3), Queen(0, 4) )
+        ) ) shouldBe false
+
+        Queen.check( Jugada( List(
+            Queen(0, 0), Queen(0, 1), Queen(0, 3), Queen(4, 4) )
+        ) ) shouldBe true
+    }
+}
 
 class PruebasSpec extends AnyFlatSpec with Matchers {
     "Test" should "work" in {
